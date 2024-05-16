45ef7048b1f41a75dd17c6a1bd6b920a2628462f
Vincent <Vincent.ahlin99@gmail.com>
2024-05-16 09:35:59 +0200

    Created endpoint and form for adding category

---
+++ app/zodSchemas/category.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const CategoryCreateSchema = z.object({
+  name: z.string().min(3).max(80),
+});
+
+export type CategoryCreate = z.infer<typeof CategoryCreateSchema>;
