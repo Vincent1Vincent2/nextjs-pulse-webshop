7a22c9dd6f1b3067178be712511ec88a743b982b
Vincent <Vincent.ahlin99@gmail.com>
2024-05-16 11:38:55 +0200

    Login with username saving as cookie

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import CategoryForm from "@/components/CategoryForm";
+import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
 
 export default function Home() {
   return (
     <main className="bg-[#F4F4F5] p-2 shadow rounded-lg container flex flex-col">
       <div className="flex flex-col gap-4 p-4 md:grid md:grid-cols-2 lg:grid-cols-3">
         <CategoryForm />
+        <RegisterForm />
+        <LoginForm />
       </div>
     </main>
   );
