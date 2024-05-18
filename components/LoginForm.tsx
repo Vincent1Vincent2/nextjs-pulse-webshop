ffcc002a2335c1168be03ace924e468cf150874b
maryamakeyf <maryamosalad98@outlook.com>
2024-05-19 00:31:02 +0200

    renderar login och logout beroende på user auth status

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,11 +1,14 @@
 "use client";
+import { authenticateUser } from "@/app/actions/authenticate";
 import { loginUser } from "@/app/actions/user";
 import { UserLogin, UserLoginSchema } from "@/app/zodSchemas/user";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import LogoutButton from "./LogoutButton";
 
 export default function LoginForm() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const form = useForm<UserLogin>({
     resolver: zodResolver(UserLoginSchema),
   });
@@ -20,7 +23,24 @@ export default function LoginForm() {
     location.reload();
   };
 
-  return (
+  useEffect(() => {
+    //kollar user auth status
+    const checkAuthentication = async () => {
+      try {
+        const authUser = await authenticateUser();
+        setIsLoggedIn(!!authUser);
+      } catch (error) {
+        console.error("Error checking authentication status:", error);
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
+  // Renderear login och logout beroende på user auth status
+  return isLoggedIn ? (
+    <LogoutButton />
+  ) : (
     <form onSubmit={form.handleSubmit(handleSubmit)}>
       <input {...form.register("email")} type="text" placeholder="Email" />
       {errors.email && <span>{errors.email.message}</span>}
@@ -31,7 +51,6 @@ export default function LoginForm() {
       />
       {errors.password && <span>{errors.password.message}</span>}
       <button>Login</button>
-      <LogoutButton />
     </form>
   );
 }
