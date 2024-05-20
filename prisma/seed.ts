1d8f021bb7c03ba7bcbfc1409155b4f2be89090b
CatAvadani <catalinaavadani@gmail.com>
2024-05-20 20:23:34 +0200

    Able to login with GitHub.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
@@ -8,24 +8,25 @@ async function main() {
 
   const category1 = await db.category.create({
     data: {
-      name: "Supplements",
+      name: 'Supplements',
     },
   });
   categories.push(category1);
 
   const category2 = await db.category.create({
     data: {
-      name: "Clothing",
+      name: 'Clothing',
     },
   });
   categories.push(category2);
 
   const product1 = await db.product.create({
     data: {
-      name: "Protein powder",
-      description: "A delicious shake filled with protein",
-      price: "132",
-      image: "protein_powder.jpg",
+      name: 'Protein powder',
+      description: 'A delicious shake filled with protein',
+      price: '132',
+      image: 'protein_powder.jpg',
+      stock: 20,
       categories: {
         connect: [
           { id: category1.id }, // Associate with cat 1
@@ -36,10 +37,11 @@ async function main() {
 
   const product2 = await db.product.create({
     data: {
-      name: "T-shirt",
-      description: "Comfortable shirt to workout in style",
-      price: new Prisma.Decimal("190"), // Price as a Decimal
-      image: "protein_powder.jpg",
+      name: 'T-shirt',
+      description: 'Comfortable shirt to workout in style',
+      price: new Prisma.Decimal('190'), // Price as a Decimal
+      image: 'protein_powder.jpg',
+      stock: 10,
       categories: {
         connect: [
           { id: category2.id }, // Using `connect` to link an existing category directly
@@ -48,13 +50,13 @@ async function main() {
     },
   });
 
-  console.log("Categories seeded:", categories);
-  console.log("Products seeded:", product1, product2);
+  console.log('Categories seeded:', categories);
+  console.log('Products seeded:', product1, product2);
 }
 
 main()
   .catch((error) => {
-    console.error("Error seeding data:", error);
+    console.error('Error seeding data:', error);
   })
   .finally(async () => {
     await db.$disconnect();
