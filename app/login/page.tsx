836d243e9ce0f03355a0c0dd6ced868169d5a039
CatAvadani <catalinaavadani@gmail.com>
2024-05-20 22:55:17 +0200

    Removed login and register pages.

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from '@/components/LoginForm';
-
-export default function Login() {
-  return (
-    <div className=' flex flex-col justify-center items-center text-black'>
-      <LoginForm />
-    </div>
-  );
-}
--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from '@/components/RegisterForm';
-
-export default function Register() {
-  return (
-    <div className=' flex flex-col justify-center items-center text-black'>
-      <RegisterForm />
-    </div>
-  );
-}
