952acbd64a3b018ddb1caa25b5e99d1cfd64a9b0
Vincent <Vincent.ahlin99@gmail.com>
2024-05-23 11:02:43 +0200

    Styling

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 import OrderForm from "@/components/OrderForm";
 export default function Checkout() {
   return (
-    <main className="bg-white m-5">
+    <main className="bg-white m-5 rounded-xl p-5">
       <OrderForm />
     </main>
   );
