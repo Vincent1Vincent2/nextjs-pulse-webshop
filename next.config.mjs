84515baca35fe11266debbbcdb1fc930f7a1f2ef
maryamakeyf <maryamosalad98@outlook.com>
2024-05-23 20:40:48 +0200

    add cdn.mmsport as hostanme

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
+        protocol: "https",
+        hostname: "i.ibb.co",
       },
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
+        protocol: "http",
+        hostname: "127.0.0.1",
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+        protocol: "https",
+        hostname: "cdn.mmsports.se",
       },
     ],
   },
@@ -20,5 +20,3 @@ const nextConfig = {
     typedRoutes: true,
   },
 };
-
-export default nextConfig;
