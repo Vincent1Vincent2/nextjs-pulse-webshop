8c1f2c4a662a7b71f697be091b2616b0e4a60ef6
Grusk2 <gustav.skepphammer@gmail.com>
2024-05-27 00:10:21 +0200

    Now it works according to the database categorys and routes to the correct category.

--- components/CategoryCards.tsx
+++ components/CategoryCards.tsx
@@ -1,27 +1,39 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { getCategories } from "@/app/actions/category";
+import Link from "next/link";
+
+interface Category {
+  id: number;
+  name: string;
+}
+
 export default function CategoryCards() {
-    const categories = [
-        { id: 1, name: "Supplaments" },
-        { id: 2, name: "Clothing" },
-        { id: 3, name: "Equipments" },
-        { id: 4, name: "Pre-Workout" },
-        { id: 5, name: "Post-Workout" },
-        { id: 6, name: "Muscle Support" },
-      ];
-    return (
-        //text-3xl md:text-5xl font-bold max-w-md md:max-w-2xl leading-loose
-        <div className="bg-black h-[350px] py-10">
-        <div className="text-white flex justify-center text-2xl font-bold">Shop By Category</div>
-        <div className="flex justify-evenly items-center gap-50 px-8 py-10">
-          {categories.map((category) => (
-            <button
-              key={category.id}
-              className="border border-orange-400 text-orange-400 rounded-md px-4 py-2 w-48  transform h-40 w-40   transition duration-500 hover:scale-125 flex justify-center items-center rounded-md"
-            >
-              <span className="text-lg font-medium">{category.name}</span>
-            </button>
-          ))}
-        </div>
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const categories = await getCategories();
+        setCategories(categories);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <div className="bg-black py-10 flex flex-col items-center">
+      <div className="text-white text-2xl font-bold mb-10">Shop By Category</div>
+      <div className="flex flex-wrap justify-center gap-40 px-4">
+        {categories.map((category) => (
+          <Link href={`/category/${category.name}/${category.id}`} key={category.id} className="flex items-center text-center justify-center border border-orange-400 text-orange-400 rounded-md h-40 w-40 transform transition duration-500 hover:scale-105">
+            <span className="text-lg font-medium">{category.name}</span>
+          </Link>
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
