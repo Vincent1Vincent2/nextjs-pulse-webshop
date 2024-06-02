e742142fb4bef401e6b5a0d9e14ff42bf3842107
CatAvadani <catalinaavadani@gmail.com>
2024-06-02 20:46:10 +0200

    Refactor the userOrders and edit-product into server components.

--- app/actions/authenticate.ts
+++ app/actions/authenticate.ts
@@ -2,20 +2,19 @@ import {auth} from "@/auth";
 import {db} from "@/prisma/db";
 
 export async function authenticateUser() {
-  try {
-    const session = await auth();
-    if (!session || !session.user || !session.user.id) {
-      return null;
-    }
-    const user = await db.user.findUnique({
-      where: {id: session.user.id},
-    });
-    if (!user) {
-      return null;
-    }
-    return user;
-  } catch (error) {
-    console.error("Error authenticating user:", error);
+  const session = await auth();
+
+  if (!session || !session.user || !session.user.id) {
     return null;
   }
+
+  const user = await db.user.findUnique({
+    where: {id: session?.user.id},
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return user;
 }
--- components/EditForm.tsx
+++ components/EditForm.tsx
@@ -1,19 +1,34 @@
 "use client";
-import {getCategories} from "@/app/actions/category";
-import {productCreate} from "@/app/actions/product";
+import {productUpdate} from "@/app/actions/product";
 import {ProductCreate, ProductCreateSchema} from "@/app/zodSchemas/product";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {Category} from "@prisma/client";
-import {useEffect, useState} from "react";
+import {Category, Product} from "@prisma/client";
+import {useEffect} from "react";
 import {useFieldArray, useForm} from "react-hook-form";
+import {toast} from "./ui/use-toast";
 
-const EditForm = () => {
+interface EditFormProps {
+  categories: Category[];
+  product?: Product;
+}
+
+const EditForm = ({categories, product}: EditFormProps) => {
   const form = useForm<ProductCreate>({
     resolver: zodResolver(ProductCreateSchema),
+    defaultValues: product || {
+      name: "",
+      description: "",
+      price: 0,
+      image: "",
+      categories: [],
+      stock: 0,
+    },
   });
   const {
     formState: {errors},
     control,
+    setError,
+    reset,
   } = form;
 
   const {fields, append, remove} = useFieldArray({
@@ -21,45 +36,106 @@ const EditForm = () => {
     name: "categories",
   });
 
-  const [categories, setCategories] = useState<Category[] | null>(null);
-
   useEffect(() => {
-    async function fetchCategories() {
-      const fetchedCategories = await getCategories();
-      setCategories(fetchedCategories);
+    if (product) {
+      reset(product);
     }
-    fetchCategories();
-  }, []);
+  }, [product, reset]);
 
   const handleSubmit = async (data: ProductCreate) => {
-    await productCreate(data);
-    form.reset();
+    await productUpdate(data, product?.id);
+    form.reset({
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      image: data.image,
+      stock: data.stock,
+      categories: data.categories,
+    });
+    toast({
+      title: "Product updated",
+      description: "Product has been updated successfully.",
+    });
   };
 
   return (
-    <form onSubmit={form.handleSubmit(handleSubmit)}>
-      <input {...form.register("name")} type="text" placeholder="Name" />
-      {errors.name && <span>{errors.name.message}</span>}
+    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="flex flex-col">
+          <input
+            {...form.register("name")}
+            type="text"
+            placeholder="Name"
+            className="p-2 border rounded text-black"
+          />
+          {errors.name && (
+            <span className="text-red-500">{errors.name.message}</span>
+          )}
+        </div>
 
-      <input
-        {...form.register("description")}
-        type="text"
-        placeholder="Description"
-      />
-      {errors.description && <span>{errors.description?.message}</span>}
+        <div className="flex flex-col">
+          <input
+            {...form.register("description")}
+            type="text"
+            placeholder="Description"
+            className="p-2 border rounded text-black"
+          />
+          {errors.description && (
+            <span className="text-red-500">{errors.description?.message}</span>
+          )}
+        </div>
 
-      <input {...form.register("price")} type="number" placeholder="Price" />
-      {errors.price && <span>{errors.price?.message}</span>}
+        <div className="flex flex-col">
+          <input
+            {...form.register("price", {valueAsNumber: true})}
+            type="number"
+            placeholder="Price"
+            className="p-2 border rounded text-black"
+          />
+          {errors.price && (
+            <span className="text-red-500">{errors.price?.message}</span>
+          )}
+        </div>
 
-      <input {...form.register("image")} type="text" placeholder="Image URL" />
-      {errors.image && <span>{errors.image?.message}</span>}
+        <div className="flex flex-col">
+          <input
+            {...form.register("image")}
+            type="text"
+            placeholder="Image URL"
+            className="p-2 border rounded text-black"
+          />
+          {errors.image && (
+            <span className="text-red-500">{errors.image?.message}</span>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <input
+            {...form.register("stock", {valueAsNumber: true})}
+            type="number"
+            placeholder="Stock"
+            className="p-2 border rounded text-black"
+          />
+          {errors.stock && (
+            <span className="text-red-500">{errors.stock?.message}</span>
+          )}
+        </div>
+      </div>
 
       <div>
+        <button
+          type="button"
+          onClick={() => append({name: "", slug: ""})}
+          className="bg-black text-white px-4 py-2 rounded w-48"
+        >
+          Add Category
+        </button>
         {fields.map((field, index) => (
-          <div key={field.id}>
+          <div key={field.id} className="flex items-center space-x-4 my-2">
             <select
               {...form.register(`categories.${index}.name` as const)}
-              defaultValue=""
+              defaultValue={field.name || ""}
+              className="p-2 border rounded text-black w-48 mt-2"
             >
               <option value="">Select a category</option>
               {categories?.map(category => (
@@ -73,13 +149,20 @@ const EditForm = () => {
             </button>
           </div>
         ))}
-        <button type="button" onClick={() => append({name: "", slug: ""})}>
-          Add Category
-        </button>
       </div>
-      {errors.categories && <span>{errors.categories.message}</span>}
+      {errors.categories && (
+        <span className="text-red-500">{errors.categories.message}</span>
+      )}
+      {errors.root && (
+        <span className="text-red-500">{errors.root.message}</span>
+      )}
 
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="bg-orange-400 text-white py-2 px-10 rounded hover:bg-orange-300 w-48"
+      >
+        Submit
+      </button>
     </form>
   );
 };
